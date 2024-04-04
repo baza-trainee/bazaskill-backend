96d73075811da88d0caa3c3d5cca31723487617a
Vladislav Shtrikun <54204831+m0o0n@users.noreply.github.com>
2024-04-04 16:45:34 +0300

    feat: candidates sevice

--- src/candidates/candidates.service.ts
+++ src/candidates/candidates.service.ts
@@ -1,15 +1,29 @@
+import { Candidate } from './entities/candidate.entity';
 import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
 import { CreateCandidateDto } from './dto/create-candidate.dto';
 import { UpdateCandidateDto } from './dto/update-candidate.dto';
+import { Repository } from 'typeorm';
+import { CandidateGraduate } from 'src/candidate_graduate/entities/candidate_graduate.entity';
 
 @Injectable()
 export class CandidatesService {
-  create(createCandidateDto: CreateCandidateDto) {
-    return 'This action adds a new candidate';
+  constructor(
+    @InjectRepository(Candidate)
+    private readonly candidateRepository: Repository<Candidate>,
+  ){}
+  async create(createCandidateDto: CreateCandidateDto) { 
+    // const {graduate} = createCandidateDto
+    const candidate = await this.candidateRepository.save(createCandidateDto); 
+    // graduate.forEach(async (item)=>{
+    //   await this.candidateGraduateRepository.save({...item, candidate_id: candidate})
+    // }) 
+    // console.log(graduate) 
+    return candidate  
   }
-
+ 
   findAll() {
-    return `This action returns all candidates`;
+    return this.candidateRepository.find();
   }
 
   findOne(id: number) {
@@ -21,6 +35,6 @@ export class CandidatesService {
   }
 
   remove(id: number) {
-    return `This action removes a #${id} candidate`;
+    return this.candidateRepository.delete(id);
   }
 }
