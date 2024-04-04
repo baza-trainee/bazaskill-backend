cf95ec960a5d4be4a9532017aba3cafdf4b23a4f
Vladislav Shtrikun <54204831+m0o0n@users.noreply.github.com>
2024-04-04 13:28:03 +0300

    feat: create-dto for candidates

--- src/candidates/dto/create-candidate.dto.ts
+++ src/candidates/dto/create-candidate.dto.ts
@@ -1,1 +1,85 @@
-export class CreateCandidateDto {}
+import { Stack } from 'src/stack/entities/stack.entity';
+import { IsArray, IsNumber, IsString } from "class-validator";
+
+export class CreateCandidateDto {
+    @IsString()
+    name_ua: string
+
+    @IsString()
+    surname_ua: string
+
+    @IsString()
+    name: string
+
+    @IsString()
+    surname: string
+
+    @IsString()
+    country: string
+
+    @IsString()
+    city: string
+
+    @IsString()
+    phone: string
+
+    @IsString()
+    email: string
+
+    @IsString()
+    linkedin: string
+
+    @IsString()
+    discord: string
+
+    @IsString()
+    telegram: string
+
+    @IsArray()
+    candidate_language: Array<{ language: string, level: string }>
+
+    @IsString()
+    work_format: string
+
+    @IsString()
+    sallary_form: string
+
+    @IsString()
+    sallary_to: string
+
+    @IsString()
+    specialization: string
+
+    @IsString()
+    cv: string
+
+    @IsArray()
+    stack: Array<string>
+
+    @IsArray()
+    graduate: Array<{
+        unversity: string,
+        graduate_sertificate: string,
+        graduate_start: string,
+        graduate_end: string,
+        unversity_grade: string,
+        unversity_specialization: string
+    }>
+
+    @IsArray()
+    cources: Array<{
+        cources_name: string,
+        cources_sertificate: string,
+        cources_specializaton: string
+        cources_start: string,
+        cources_end: string
+    }>
+
+    @IsArray()
+    baza_experience: Array<{
+        role: string,
+        project_name: string,
+        project_duration: string
+    }>
+
+}
