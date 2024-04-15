78217c6c42d4b9312fed7820c23a8b7847c8f710
cold <kholodihor@gmail.com>
2024-04-15 09:44:24 +0300

    fix: forgot password endpoint

--- src/baza_experience/entities/baza_experience.entity.ts
+++ src/baza_experience/entities/baza_experience.entity.ts
@@ -1,23 +1,31 @@
 import { Candidate } from 'src/candidates/entities/candidate.entity';
-import { Specialization } from "src/specialization/entities/specialization.entity";
-import { Column, Entity, JoinColumn, ManyToOne, OneToOne, PrimaryGeneratedColumn } from "typeorm";
+import { Specialization } from 'src/specialization/entities/specialization.entity';
+import {
+  Column,
+  Entity,
+  JoinColumn,
+  ManyToOne,
+  PrimaryGeneratedColumn,
+} from 'typeorm';
 
 @Entity('baza_experience')
 export class BazaExperience {
-    @PrimaryGeneratedColumn()
-    id: number
+  @PrimaryGeneratedColumn()
+  id: number;
 
-    @ManyToOne(() => Specialization, {onDelete: 'CASCADE'})
-    @JoinColumn()
-    specialization_id: Specialization
+  @ManyToOne(() => Specialization, { onDelete: 'CASCADE' })
+  @JoinColumn()
+  specialization_id: Specialization;
 
-    @Column()
-    project_name: string
+  @Column()
+  project_name: string;
 
-    @Column()
-    project_duration: string
+  @Column()
+  project_duration: string;
 
-    @ManyToOne(()=> Candidate, (candidate)=>candidate.baza_experience, {onDelete: 'CASCADE'})
-    @JoinColumn({name: 'candidate_id'})
-    candidate_id: Candidate
+  @ManyToOne(() => Candidate, (candidate) => candidate.baza_experience, {
+    onDelete: 'CASCADE',
+  })
+  @JoinColumn({ name: 'candidate_id' })
+  candidate_id: Candidate;
 }
--- src/candidates/candidates.controller.ts
+++ src/candidates/candidates.controller.ts
@@ -1,4 +1,12 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/common';
+import {
+  Controller,
+  Get,
+  Post,
+  Body,
+  Patch,
+  Param,
+  Delete,
+} from '@nestjs/common';
 import { CandidatesService } from './candidates.service';
 import { CreateCandidateDto } from './dto/create-candidate.dto';
 import { UpdateCandidateDto } from './dto/update-candidate.dto';
@@ -23,7 +31,10 @@ export class CandidatesController {
   }
 
   @Patch(':id')
-  update(@Param('id') id: string, @Body() updateCandidateDto: UpdateCandidateDto) {
+  update(
+    @Param('id') id: string,
+    @Body() updateCandidateDto: UpdateCandidateDto,
+  ) {
     return this.candidatesService.update(+id, updateCandidateDto);
   }
 
